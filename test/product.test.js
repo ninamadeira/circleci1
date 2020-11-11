212080a36c0baa3c0ef103e956dd1bc84e54214e
Nina Rosa Madeira <nina.madeira@gmail.com>
2020-11-11 00:01:16 -0300

    circleci1

--- test/product.test.js
+++ test/product.test.js
@@ -12,5 +12,5 @@ test('deve validar baixa de estoque da venda de uma unidade', () => {
 test('deve aceitar a venda de mais do que uma unidade', () =>{
     let produto = new Product('Celular', 500.00, 900.00, 10);
     sell(produto, 3);
-    expect(produto.stock).toBe(7);
+    expect(produto.stock).toBe(9);
 })
